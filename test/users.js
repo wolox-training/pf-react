7e682e3a5afc2aba0284050babbdbb048a9f46a0
Pablo Farias <pfariaz@gmail.com>
2017-11-30 11:14:46 -0300

    fix timeout test

--- test/users.js
+++ test/users.js
@@ -134,8 +134,8 @@ describe('users', () => {
           .then(response => {
             response.should.have.status(200);
             response.should.be.json;
-          })
-          .then(() => done());
+            done();
+          });
       });
     });
   });
